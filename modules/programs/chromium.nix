bc051407c6e3068f68b2628ceb22113a488b616e
andre4ik3 <andre4ik3@fastmail.com>
2026-07-07 04:00:12 +0000

    chromium: add helium

--- modules/programs/chromium.nix
+++ modules/programs/chromium.nix
@@ -18,6 +18,7 @@ let
     brave-origin = "Brave Origin";
     vivaldi = "Vivaldi Browser";
     microsoft-edge = "Microsoft Edge";
+    helium = "Helium";
   };
 
   plasmaSupportedBrowsers = [
@@ -237,6 +238,7 @@ let
         brave = "BraveSoftware/Brave-Browser";
         brave-origin = "BraveSoftware/Brave-Origin";
         microsoft-edge = "Microsoft Edge";
+        helium = "net.imput.helium";
       };
 
       linuxDirs = {
